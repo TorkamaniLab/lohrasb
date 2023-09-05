bd66f39387cd9960da683836881887c7267fa5e9
drhosseinjavedani <h.javedani@gmail.com>
2023-09-04 20:40:01 -0700

    change : jenkins to git actions

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,9 @@
 import nox
 
+# Default sessions to run when no sessions are specified.
 nox.options.sessions = ["tests_lohrasb", "lint_lohrasb"]
+
+# List of test files to be executed.
 test_files = [
     "tests/test_optimize_by_tune.py",
     "tests/test_optimize_by_tunegridsearchcv.py",
@@ -10,24 +13,21 @@ test_files = [
     "tests/test_optimize_by_gridsearchcv.py",
     "tests/test_optimize_by_randomsearchcv.py",
 ]
-@nox.session  # Removed python=PYTHON_VERSIONS
+
+@nox.session
 def tests_lohrasb(session):
-    # The list of test files to be executed
-    """Install test dependencies."""
+    """Install test dependencies and run pytest for all test files."""
     session.install("-r", "requirements_test.txt")
-    session.run("pytest", test_files[0])
-    session.run("pytest", test_files[1])
-    session.run("pytest", test_files[2])
-    session.run("pytest", test_files[3])
-    session.run("pytest", test_files[4])
-    session.run("pytest", test_files[5])
-    session.run("pytest", test_files[6])
+    
+    for test_file in test_files:
+        session.run("pytest", test_file)
 
 @nox.session
 def lint_lohrasb(session):
-    """Run lint session using nox"""
-    # Install linters
+    """Run lint session using nox."""
+    # Install linters.
     session.install("black", "isort")
-    # Run isort and black
+    
+    # Run isort and black.
     session.run("isort", "./lohrasb/")
     session.run("black", "./lohrasb/")
